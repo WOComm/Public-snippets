e135e19ae213df45b65312e8f71ecbce1685d954
Vince Wooll <sales@jomres.net>
2017-11-06 14:46:17 +0100

    Installer updated, plus various webhook tools updated.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Public-snippets
-This repo is intended for snippets of code that we want to share with our users.
--- dev_tool_webhook_responder_script/simulation_webhook_responder/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$syslog_server = '127.0.0.1'; // Syslog server's ip or domain
-$syslog_server_port = 514; // Syslog server's port
-
-$server = 'http://localhost/joomla_portal/jomres/api/'; // The url to the Jomres installation 
-$client_id = 'HwclaHGHTfPWnho'; // The Client ID on Jomres for this API key pair
-$client_secret = 'lxTzWPhGzAzvcVlmzIDhmMnuCGSomRxsPJTlvqVWLrthllnsUQ'; // The Client Secret on Jomres for this API key pair
